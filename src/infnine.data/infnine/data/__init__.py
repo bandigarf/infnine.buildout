d2f5f2c832478d0752bf673efa6949fea1d33b7d
Andrija Prčić <andrija.prcic@gmail.com>
2008-07-19 17:26:04 +0200

    Publication: Default add role assigned to manager

--- src/infnine.data/infnine/data/__init__.py
+++ src/infnine.data/infnine/data/__init__.py
@@ -8,6 +8,7 @@ from Products.CMFCore.permissions import setDefaultRoles
 _ = MessageFactory('infnine.data')
 
 setDefaultRoles('Infnine Data: Add Person', ('Manager',))
+setDefaultRoles('Infnine Data: Add Publication', ('Manager',))
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
