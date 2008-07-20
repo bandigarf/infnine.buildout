6c8996f342bd98b95e74445a5998c4a1e8867bf4
Andrija Prčić <andrija.prcic@gmail.com>
2008-07-20 16:27:25 +0200

    infnine.policy: dependency on infnine.data + tests

--- src/infnine.policy/infnine/policy/tests/base.py
+++ src/infnine.policy/infnine/policy/tests/base.py
@@ -4,6 +4,8 @@ from Testing import ZopeTestCase as ztc
 from Products.PloneTestCase import PloneTestCase as ptc
 from Products.PloneTestCase.layer import onsetup
 
+ztc.installProduct('infnine.data')
+
 @onsetup
 def setup_infnine_policy():
     """Set up the additional products required for the Infnine site policy.
