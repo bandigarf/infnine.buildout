524bb7d39894b3a09b182c211b5da24092453493
Andrija Prčić <andrija.prcic@gmail.com>
2008-07-20 13:09:07 +0200

    infnine.policy product created

---
+++ src/infnine.policy/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='infnine.policy',
+      version=version,
+      description="Infnine Site Policy",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Andrija Pr\xc4\x8di\xc4\x87',
+      author_email='andrija.prcic@gmail.com',
+      url='http://www9.cs.tum.edu',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['infnine'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
