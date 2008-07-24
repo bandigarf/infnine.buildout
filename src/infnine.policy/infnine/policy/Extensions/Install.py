3bbbd22ff2e429649bdceb09cc3e82d121df7e1e
Andrija Prčić <andrija.prcic@gmail.com>
2008-07-24 15:44:07 +0200

    infnine.policy: revised to include the theme package

--- src/infnine.policy/infnine/policy/Extensions/Install.py
+++ src/infnine.policy/infnine/policy/Extensions/Install.py
@@ -1,15 +1,30 @@
 import transaction
 from Products.CMFCore.utils import getToolByName
 
-PRODUCT_DEPENDENCIES = ('infnine.data',)
+PRODUCT_DEPENDENCIES = ('infnine.theme','infnine.content',)
+
 EXTENSION_PROFILES = ('infnine.policy:default',)
 
 def install(self, reinstall=False):
+    """Install a set of products (which themselves may either use Install.py
+    or GenericSetup extension profiles for their configuration) and then
+    install a set of extension profiles.
+
+    One of the extension profiles we install is that of this product. This
+    works because an Install.py installation script (such as this one) takes
+    precedence over extension profiles for the same product in 
+    portal_quickinstaller. 
+
+    We do this because it is not possible to install other products during
+    the execution of an extension profile (i.e. we cannot do this during
+    the importVarious step for this profile).
+    """
+
     portal_quickinstaller = getToolByName(self, 'portal_quickinstaller')
     portal_setup = getToolByName(self, 'portal_setup')
 
     for product in PRODUCT_DEPENDENCIES:
-        if reinstall and portal_qiuckinstaller.isProductInstalled(product):
+        if reinstall and portal_quickinstaller.isProductInstalled(product):
             portal_quickinstaller.reinstallProducts([product])
             transaction.savepoint()
         elif not portal_quickinstaller.isProductInstalled(product):
@@ -21,4 +36,3 @@ def install(self, reinstall=False):
         product_name = extension_id.split(':')[0]
         portal_quickinstaller.notifyInstalled(product_name)
         transaction.savepoint()
-
