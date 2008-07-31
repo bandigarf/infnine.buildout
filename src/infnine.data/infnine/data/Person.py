61308ddfd28f8a3f9509062d7937f4fe479474d6
Andrija Prčić <andrija.prcic@gmail.com>
2008-07-31 16:06:15 +0200

    Person: additional fields from the UML diagrams + TAL for publications field

--- src/infnine.data/infnine/data/Person.py
+++ src/infnine.data/infnine/data/Person.py
@@ -1,5 +1,5 @@
 from zope.interface import Interface
-from zope.schema import TextLine, Text
+from zope.schema import TextLine, Text, Choice
 
 class IPerson(Interface):
     """A person
@@ -9,6 +9,15 @@ class IPerson(Interface):
             title=u"Name",
             description=u"Display name of the person",
             required=True,
+            )
+
+    status = Choice(
+            title=u"Status",
+            description=u"Status of the person",
+            required=True,
+            default="Other",
+            values=("Professor", "Secretariat", "Researcher", "Visiting Researcher",
+                    "Alumnus", "Administration", "Student Member", "External", "Other"),
             )
 
     position = TextLine(
@@ -43,6 +52,36 @@ class IPerson(Interface):
     office = TextLine(
             title=u"Office",
             description=u"Office number",
+            required=False,
+            )
+
+    publications = Text(
+            title=u"Publications",
+            description=u"",
+            required=False,
+            )
+
+    research_projects_current = Text(
+            title=u"Current research projects",
+            description=u"Research projects the person is currently active in",
+            required=False,
+            )
+
+    research_projects_former = Text(
+            title=u"Former research projects",
+            description=u"Research projects the person was active in",
+            required=False,
+            )
+
+    teaching = Text(
+            title=u"Teaching",
+            description=u"",
+            required=False,
+            )
+
+    students = Text(
+            title=u"Students",
+            description=u"",
             required=False,
             )
 
--- src/infnine.data/infnine/data/content/person.py
+++ src/infnine.data/infnine/data/content/person.py
@@ -17,12 +17,18 @@ class PersonContent(Container):
 
     portal_type = "Person"
 
+    status = FieldProperty(IPerson['status'])
     position = FieldProperty(IPerson['position'])
     department = FieldProperty(IPerson['department'])
     email = FieldProperty(IPerson['email'])
     telephone = FieldProperty(IPerson['telephone'])
     fax = FieldProperty(IPerson['fax'])
     office = FieldProperty(IPerson['office'])
+    publications = FieldProperty(IPerson['publications'])
+    research_projects_current = FieldProperty(IPerson['research_projects_current'])
+    research_projects_former = FieldProperty(IPerson['research_projects_former'])
+    teaching = FieldProperty(IPerson['teaching'])
+    students = FieldProperty(IPerson['students'])
     misc = FieldProperty(IPerson['misc'])
 
 factory = Factory(
