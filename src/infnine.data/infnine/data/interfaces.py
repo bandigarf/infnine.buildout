68bfb98cdb7dc2d2900dff44f1115f62449a857d
Andrija Prčić <andrija.prcic@gmail.com>
2008-08-10 21:34:16 +0200

    Revert "AlumniPage: for people section, for listing only people with status 'Alumnus'"
    This reverts commit fa4e614a40b6338dd9ff74cdbbc50daceaf87b07.

--- src/infnine.data/infnine/data/interfaces.py
+++ src/infnine.data/infnine/data/interfaces.py
@@ -1,6 +1,5 @@
 from Person import IPerson
 from PeoplePage import IPeoplePage
-from AlumniPage import IAlumniPage
 from Publication import IPublication
 from ResearchProject import IResearchProject
 from ResearchPage import IResearchPage
