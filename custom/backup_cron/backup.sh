f14033a3871a7f42a79498ffa5ff0eeb4ea47300
Andrija Prcic <andrija.prcic@gmail.com>
2008-09-05 19:46:13 +0200

    Research-map areas coordinates updated, cron jobs have now full path, .researchprojectsummaryeven in main.css have text-align set to justify.

--- custom/backup_cron/backup.sh
+++ custom/backup_cron/backup.sh
@@ -1,1 +1,1 @@
-${INFNINEHOME}/bin/repozo -B -f ../../var/filestorage/Data.fs -z -v -r ../../var/filestorage/backup/
+/usr/prakt/p098/infnine.buildout/bin/repozo -B -f /usr/prakt/p098/infnine.buildout/var/filestorage/Data.fs -z -v -r /usr/prakt/p098/infnine.buildout/var/filestorage/backup/
