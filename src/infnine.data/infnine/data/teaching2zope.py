116e11972be94026c31b7954d07cc2711c719b06
Andrija Prčić <andrija.prcic@gmail.com>
2008-10-16 17:37:40 +0200

    teaching2zope: gets SQL data from readdb script and creates ZODB objects in the teaching section

---
+++ src/infnine.data/infnine/data/teaching2zope.py
@@ -0,0 +1,74 @@
+
+# online version
+#from readdb import getEvents
+#semester = semester_id = 'WS2008'
+#semester = getEvents(semester_id)
+
+# offline version
+import datetime
+semester = {128: {'Language': 0, 'Url': 'http://www9.in.tum.de/praktika/awbs_datamining.WS08/', 'Professor': (('Prof. Michael Beetz, PhD',),), 'Module': (('Praktikum im Bereich Praktische Informatik', 'IN8902'),), 'German Title': 'Knowledge Discovery', 'Date': [{}], 'Instructor': (('Nicolai von Hoyningen-Huene',),), 'Type': 'Praktikum', 'English Title': 'Knowledge Discovery'}, 129: {'Language': 0, 'Url': 'http://www9.in.tum.de/praktika/kr.ws08/', 'Professor': (('Prof. Michael Beetz, PhD',),), 'Module': (('Praktikum im Bereich Praktische Informatik', 'IN8902'),), 'German Title': 'Wissensrepr\xc3\xa4sentation f\xc3\xbcr kognitive technische Systeme', 'Date': [{'StartDate': None, 'EndHour': 14, 'EndDate': None, 'Room': 'MI 02.09.023', 'EndMinute': 30, 'StartMinute': 0, 'Type': '---', 'Day': 'Donnerstag', 'StartHour': 13}], 'Instructor': (('Moritz Tenorth',),), 'Type': 'Praktikum', 'English Title': 'Knowledge representations for cognitive technical systems'}, 107: {'Language': 0, 'Url': 'http://wwwradig.in.tum.de/praktika/impro++.WS08/', 'Professor': (('Prof. Dr. Bernd Radig',),), 'Module': (('Praktikum im Bereich Praktische Informatik', 'IN8902'),), 'German Title': 'Bildverarbeitung in C++', 'Date': [{}], 'Instructor': (('Dr. Matthias Wimmer',), ('Christoph Mayer',), ('Zahid Riaz',)), 'Type': 'Praktikum', 'English Title': 'Image Processing in C++'}, 108: {'Language': 0, 'Url': 'http://wwwradig.in.tum.de/seminare/hs.WS08.ianalysis/', 'Professor': (('Prof. Dr. Bernd Radig',),), 'Module': (('Master-Seminar', 'IN2107'),), 'German Title': 'Bildverstehen - Visuelle Analyse von menschlichen Gesichtern', 'Date': [{'StartDate': datetime.date(2008, 10, 20), 'EndHour': 16, 'EndDate': datetime.date(2009, 3, 13), 'Room': 'MI 02.09.023', 'EndMinute': 30, 'StartMinute': 0, 'Type': '---', 'Day': 'Donnerstag', 'StartHour': 15}], 'Instructor': (('Dr. Matthias Wimmer',), ('Christoph Mayer',), ('Zahid Riaz',)), 'Type': 'Master-Seminar', 'English Title': 'Image Understanding - Face Image Analysis'}, 109: {'Language': 0, 'Url': 'http://wwwradig.in.tum.de/praktika/awbs_is.WS08/', 'Professor': (('Prof. Michael Beetz, PhD',),), 'Module': (('Praktikum im Bereich Praktische Informatik', 'IN8902'),), 'German Title': 'KI-basierte Robotersteuerung', 'Date': [{'StartDate': None, 'EndHour': 16, 'EndDate': None, 'Room': 'MI 02.09.023', 'EndMinute': 0, 'StartMinute': 30, 'Type': '---', 'Day': 'Dienstag', 'StartHour': 14}], 'Instructor': (('Thomas R\xc3\xbchr',), ('Lorenz M\xc3\xb6senlechner',)), 'Type': 'Praktikum', 'English Title': 'AI-based Robot Control'}, 110: {'Language': 0, 'Url': 'http://www9.in.tum.de/skripten/WS0809BV/', 'Professor': (('Prof. Dr. Bernd Radig',),), 'Module': (('Bildverstehen', 'IN2016'),), 'German Title': 'Bildverstehen', 'Date': [{}], 'Instructor': (), 'Type': 'Wahlpflichtvorlesung', 'English Title': 'Image Understanding'}, 111: {'Language': 1, 'Url': 'http://www9.cs.tum.edu/praktika/robotarm.WS08/', 'Professor': (('Prof. Michael Beetz, PhD',),), 'Module': (('Praktikum im Bereich Praktische Informatik', 'IN8902'),), 'German Title': 'Roboter-Praktikum: Steuerung von Robotermanipulatoren', 'Date': [{'StartDate': datetime.date(2008, 10, 21), 'EndHour': 15, 'EndDate': datetime.date(2009, 2, 3), 'Room': 'MI 02.09.023', 'EndMinute': 0, 'StartMinute': 15, 'Type': '---', 'Day': 'Dienstag', 'StartHour': 13}], 'Instructor': (('Alexis Maldonado',), ('Andreas Fedrizzi',)), 'Type': 'Praktikum', 'English Title': 'Roboter-Praktikum: Controlling Robot Manipulators'}, 112: {'Language': 0, 'Url': 'http://www9.in.tum.de/vorlesungen/ki.WS08/', 'Professor': (('Prof. Michael Beetz, PhD',),), 'Module': (('Grundlagen der K\xc3\xbcnstlichen Intelligenz', 'IN2062'),), 'German Title': 'Grundlagen der K\xc3\xbcnstlichen Intelligenz', 'Date': [{'StartDate': None, 'EndHour': 13, 'EndDate': None, 'Room': 'MI HS 2', 'EndMinute': 45, 'StartMinute': 15, 'Type': '---', 'Day': 'Dienstag', 'StartHour': 12}], 'Instructor': (), 'Type': 'Wahlpflichtvorlesung', 'English Title': 'Introduction to Artificial Intelligence'}, 113: {'Language': 1, 'Url': 'http://www.pe.mw.tum.de/index.php?inhalt=seminare&typ=2', 'Professor': (('Prof. Michael Beetz, PhD',),), 'Module': (('Praktikum im Bereich Praktische Informatik', 'IN8902'),), 'German Title': 'Innovation@CoTeSys', 'Date': [{}], 'Instructor': (('Federico Ruiz',),), 'Type': 'Praktikum', 'English Title': 'Innovation@CoTeSys'}, 114: {'Language': 1, 'Url': 'http://wwwradig.in.tum.de/research/MQM/teaching/KBSIA/index.php', 'Professor': (('Prof. Dr. Peter Struss',),), 'Module': (('Wissensbasierte Systeme f\xc3\xbcr industrielle Anwendungen', 'IN2071'),), 'German Title': 'Wissensbasierte Systeme f\xc3\xbcr industrielle Anwendungen', 'Date': [{'StartDate': None, 'EndHour': 15, 'EndDate': None, 'Room': 'MI 02.09.023', 'EndMinute': 55, 'StartMinute': 30, 'Type': '---', 'Day': 'Montag', 'StartHour': 13}], 'Instructor': (), 'Type': 'Vertiefungsvorlesung', 'English Title': 'Knowledge-based Systems for Industrial Applications'}, 118: {'Language': 0, 'Url': 'http://www9.in.tum.de/seminare/hs.WS08.bildverstehen', 'Professor': (('Prof. Dr. Bernd Radig',),), 'Module': (('Hauptseminar', 'IN8901'),), 'German Title': 'Bildverstehen', 'Date': [{'StartDate': None, 'EndHour': 16, 'EndDate': None, 'Room': 'MI 02.09.023', 'EndMinute': 30, 'StartMinute': 0, 'Type': '---', 'Day': 'Mittwoch', 'StartHour': 15}], 'Instructor': (('Jan Bandouch',),), 'Type': 'Hauptseminar', 'English Title': 'Image Understanding'}, 119: {'Language': 1, 'Url': 'http://www9.in.tum.de/seminare/doktorandenseminar/', 'Professor': (('Prof. Dr. Bernd Radig',), ('Prof. Michael Beetz, PhD',)), 'Module': (('Doktorandenseminar', 'IN2137'),), 'German Title': 'Bildverstehen und intelligente autonome Systeme', 'Date': [{'StartDate': None, 'EndHour': 12, 'EndDate': None, 'Room': 'MI 02.09.023', 'EndMinute': 0, 'StartMinute': 0, 'Type': '---', 'Day': 'Freitag', 'StartHour': 11}], 'Instructor': (), 'Type': 'Doktorandenseminar', 'English Title': 'Image Understanding and Intelligent Autonomous Systems'}, 120: {'Language': 0, 'Url': 'http://www9.in.tum.de/seminare/oberseminar/', 'Professor': (('Prof. Dr. Bernd Radig',),), 'Module': (('Oberseminar', 'IN2122'),), 'German Title': 'Bildverstehen', 'Date': [{'StartDate': None, 'EndHour': 12, 'EndDate': None, 'Room': 'MI 02.09.023', 'EndMinute': 0, 'StartMinute': 0, 'Type': '---', 'Day': 'Mittwoch', 'StartHour': 11}], 'Instructor': (), 'Type': 'Oberseminar', 'English Title': 'Image Understanding'}, 121: {'Language': 0, 'Url': 'http://www9.in.tum.de/seminare/oberseminar/', 'Professor': (('Prof. Michael Beetz, PhD',),), 'Module': (('Oberseminar', 'IN2122'),), 'German Title': 'Wissensbasierte Systeme', 'Date': [{'StartDate': None, 'EndHour': 12, 'EndDate': None, 'Room': 'MI 02.09.023', 'EndMinute': 0, 'StartMinute': 0, 'Type': '---', 'Day': 'Mittwoch', 'StartHour': 11}], 'Instructor': (), 'Type': 'Oberseminar', 'English Title': 'Knowledge-Based Systems'}, 122: {'Language': 0, 'Url': 'http://www9.in.tum.de/seminare/hs.WS08.IS/', 'Professor': (('Prof. Michael Beetz, PhD',),), 'Module': (('Hauptseminar', 'IN8901'),), 'German Title': 'Intelligente Systeme', 'Date': [{'StartDate': datetime.date(2008, 11, 18), 'EndHour': 11, 'EndDate': datetime.date(2008, 12, 2), 'Room': 'MI 02.09.023', 'EndMinute': 45, 'StartMinute': 15, 'Type': '---', 'Day': 'Dienstag', 'StartHour': 10}], 'Instructor': (('Andreas Fedrizzi',),), 'Type': 'Hauptseminar', 'English Title': 'Intelligent Systems'}, 123: {'Language': 1, 'Url': 'http://www9.in.tum.de/seminare/ps.WS08.IS/', 'Professor': (('Prof. Michael Beetz, PhD',),), 'Module': (('Proseminar', 'IN0013'),), 'German Title': 'Intelligente Systeme', 'Date': [{'StartDate': None, 'EndHour': 15, 'EndDate': None, 'Room': 'MI 02.09.023', 'EndMinute': 0, 'StartMinute': 0, 'Type': '---', 'Day': 'Mittwoch', 'StartHour': 13}], 'Instructor': (('Radu Bogdan Rusu',), ('Mihai Dolha',), ('Zoltan-Csaba Marton',)), 'Type': 'Proseminar', 'English Title': 'Intelligent Systems'}, 124: {'Language': 1, 'Url': 'http://wwwradig.in.tum.de/praktika/seie.WS08/', 'Professor': (('Prof. Michael Beetz, PhD',),), 'Module': (('Praktikum im Bereich Praktische Informatik', 'IN8902'),), 'German Title': 'Sensorgest\xc3\xbctzte intelligente Umgebungen', 'Date': [{}], 'Instructor': (('Radu Bogdan Rusu',), ('Mihai Dolha',), ('Zoltan-Csaba Marton',)), 'Type': 'Praktikum', 'English Title': 'Sensor-enabled Intelligent Environments'}, 127: {'Language': 1, 'Url': 'http://www9.in.tum.de/seminare/ps.WS08.gdbv/index.php', 'Professor': (('Prof. Dr. Bernd Radig',),), 'Module': (('Proseminar', 'IN0013'),), 'German Title': 'Grundlagen der digitalen Bildverarbeitung', 'Date': [{'StartDate': None, 'EndHour': 12, 'EndDate': None, 'Room': 'MI 02.09.023', 'EndMinute': 0, 'StartMinute': 0, 'Type': '---', 'Day': 'Donnerstag', 'StartHour': 10}], 'Instructor': (('Francisco Siles',), ('Zahid Riaz',)), 'Type': 'Proseminar', 'English Title': 'Fundamentals of Digital Image Processing'}}
+
+def keymap(event_type, key):
+    keymap_generic = {
+        'English Title': 'title',
+        'German Title': 'title_german',
+        #description
+        #details
+        #foobar_type
+        'Professor': 'professor',
+        'Instructor': 'instructor',
+#        'Date': 'date_place',
+        #foobar_language
+        'Module': 'module',
+        #term
+        'Url': 'url',
+    }
+    if key in keymap_generic.keys():
+        return keymap_generic[key]
+    if key in ('Type', 'Language'):
+        return event_type + '_' + key.lower()
+    print "!!! Don't know how to handle key:", key
+    return None
+
+def setvalue(zodb_object, key, value):
+    eventtype_prefix = {
+        'Lecture': 'lecture',
+        'Practical Course': 'practicalcourse',
+        'Seminar': 'seminar'
+    }
+    zodb_key = keymap(eventtype_prefix[zodb_object.meta_type], key)
+    if zodb_key:
+        zodb_object.__setattr__(zodb_key, str(value).decode('utf-8'))
+
+for id in semester.keys():
+    if ('app' in dir()) and (app != None):
+        teaching = app.infnine.teaching
+        print "Running in Zope, creating objects in ZODB..."
+        if not teaching.hasObject(`id`):
+            print "Entry", `id`, "not yet in ZODB, creating"
+            admin = app.acl_users.getUser('admin')
+            import AccessControl
+            AccessControl.SecurityManagement.newSecurityManager(None, admin)
+            from Testing.makerequest import makerequest
+            app = makerequest(app)
+
+            teaching.invokeFactory(event_type, `id`)
+
+        event_zodb = teaching.__getitem__(`id`)
+        event = semester[id]
+        t = semester[id]['Type']
+        if (t == 'Wahlpflichtvorlesung') or (t == 'Vertiefungsvorlesung'):
+            event_type = 'Lecture'
+        elif t == 'Praktikum':
+            event_type = 'Practical Course'
+        else:
+            event_type = 'Seminar'
+
+        keys = event.keys()
+        print "Updating", `id`, keys
+        for key in keys:
+            setvalue(event_zodb, key, event[key])
+
+        event_zodb.reindexObject()
+        import transaction
+        transaction.commit()
