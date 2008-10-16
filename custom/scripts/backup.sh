5e83027cfe121a4e22193e0dc8a2501288310759
pangercic <pangercic@lapradig41.informatik.tu-muenchen.de>
2008-10-16 13:52:58 +0200

    correct path

--- custom/scripts/backup.sh
+++ custom/scripts/backup.sh
@@ -3,7 +3,7 @@
 username=`id -nu`
 if [ "$username" = "pangercic" ]
 then
-/home/pangercic/programing/inf9_webpage/infnine.buildout/bin/repozo -B -f /home/pangercic/programing/inf9_webpage/infnine.buildout/var/filestorage/Data.fs -z -v -r /home/pangercic/programing/inf9_webpage/infnine/infnine.buildout/var/filestorage/backup/
+/home/pangercic/programing/inf9_webpage/infnine.buildout/bin/repozo -B -f /home/pangercic/programing/inf9_webpage/infnine.buildout/var/filestorage/Data.fs -z -v -r /home/pangercic/programing/inf9_webpage/infnine.buildout/var/filestorage/backup/
 elif [ "$username" = "infnine" ]
 then	
 /usr/proj/infnine/infnine.buildout/bin/repozo -B -f /usr/proj/infnine/infnine.buildout/var/filestorage/Data.fs -z -v -r /usr/proj/infnine/infnine.buildout/var/filestorage/backup/
