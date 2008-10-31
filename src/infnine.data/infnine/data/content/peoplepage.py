f477372c5524a9c50752df1e09476dc3e3d4fedc
pangercic <pangercic@lapradig41.informatik.tu-muenchen.de>
2008-10-31 15:49:20 +0100

    Research staff, Visiting Researchers and Student as sorted lists (sorting script implemented in peoplepage.py)

--- src/infnine.data/infnine/data/content/peoplepage.py
+++ src/infnine.data/infnine/data/content/peoplepage.py
@@ -8,6 +8,14 @@ from zope.schema.fieldproperty import FieldProperty
 
 from infnine.data.interfaces import IPeoplePage
 
+from DocumentTemplate import sequence
+
+def sortPeople(self, peopleListUnsorted):
+    peopleListSorted = []
+    peopleListSorted = sequence.sort(peopleListUnsorted, (('id', 'nocase', 'asc'),))    
+    return peopleListSorted
+    
+
 class PeoplePageContent(Item):
     """Page to list people
     """
@@ -16,7 +24,10 @@ class PeoplePageContent(Item):
             INameFromTitle)
 
     portal_type = "People Page"
+    pLS = sortPeople
 
 factory = Factory(
         PeoplePageContent,
         )
+
+
