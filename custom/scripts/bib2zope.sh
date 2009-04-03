1d4feefec5f67ae130a3a467234c18c90480edc2
pangercic <pangercic@lapradig41.informatik.tu-muenchen.de>
2009-04-03 12:19:24 +0200

    bib2zope, bibsplitter run in one bash script

--- custom/scripts/bib2zope.sh
+++ custom/scripts/bib2zope.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 direct=`pwd | rev | awk -F \/ '{print $1}' | rev`
 username=`id -nu`
-if [ "$direct" = "bibliography" ]
-then
+#if [ "$direct" = "bibliography" ]
+#then
 if [ "$username" = "pangercic" ]
 then
 /home/pangercic/programing/inf9_webpage/infnine.buildout/bin/zope-secondary run /home/pangercic/programing/inf9_webpage/infnine.buildout/src/infnine.data/infnine/data/bib2zope.py
 elif [ "$username" = "infnine" ]
 then	
 /usr/proj/infnine/infnine.buildout/bin/zope-secondary run /usr/proj/infnine/infnine.buildout/src/infnine.data/infnine/data/bib2zope.py
+/usr/proj/infnine/infnine.buildout/custom/scripts/bibsplitter.py
 else
     echo "I do not know YOU."
 fi
-else
-echo "run script from bibliography directory"
-fi
+#else
+#echo "run script from bibliography directory"
+#fi
 
